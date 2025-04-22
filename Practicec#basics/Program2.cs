c07ea66c3ad7e2e29c2c89331c70721856efeca5
mishraayush925 <61752344+mishraayush925@users.noreply.github.com>
2025-04-23 02:40:54 +0530

    practice codes for extension methods

--- Practicec#basics/Program2.cs
+++ Practicec#basics/Program2.cs
@@ -6,11 +6,93 @@ using System.Threading.Tasks;
 
 namespace Practicec_basics
 {
+    public delegate void PrinterDelegate(string str, int n);
     internal class Program2
     {
-        public static void Main(string[] args)
+        public static void Printbefore(string str , int n)
         {
-            Console.WriteLine("hello program2");
+            Console.WriteLine(str + " needs to be printed " + n +" times");
+        }
+
+        public static void PrinterAfter(string str, int n)
+        {
+            Console.WriteLine(str + " has been  printed " + n + " times");
+        }
+        public static void PrintaLine()
+        {
+            Console.WriteLine("just print a line");
+        }
+        public static void PrintaLine(int a)
+        {
+            Console.WriteLine("just print value of  a : "+a);
+        }
+        public static void DoSomething(Action action)
+        {
+            Console.WriteLine("Before action");
+            action(); // Invoke the passed-in method
+            Console.WriteLine("After action");
+        }
+        public static void DoSomething(Action<int, int> action)
+        {
+            Console.WriteLine("Before action");
+            action(5, 7); // Invoke the passed-in method
+            Thread thread = new Thread(()=>PrintaLine(7));
+            thread.Name = "my thread123";
+           // thread.IsBackground = true;
+            thread.Start();
+            Console.WriteLine("After action");
+        }
+        public static void DoSomething(Func<int, int,int> func)
+        {
+            Console.WriteLine("Before action");
+            func(5, 7); // Invoke the passed-in method
+            Thread thread = new Thread(() => PrintaLine(7));
+            thread.Name = "my thread123";
+            // thread.IsBackground = true;
+            thread.Start();
+            Console.WriteLine("After action");
+        }
+
+        public static void DoSomething(PrinterDelegate p)
+        {
+            Console.WriteLine("Before action for printer delegates");
+            p("akm", 7); // Invoke the passed-in method
+            //Thread thread = new Thread(() => PrintaLine(7));
+            //thread.Name = "my thread123";
+            //// thread.IsBackground = true;
+            //thread.Start();
+            Console.WriteLine("After action");
+        }
+        public static void Main()
+        {
+            void f1()
+            {
+                Console.WriteLine("from f1");
+            }
+            Action act = () => Console.WriteLine("Hello from lambda");
+             act += () => Console.WriteLine("Hello from lambda2");
+            act += f1;
+            Func<int, int, int> func2 = (a, b) =>
+            {
+
+                a = a + 5;
+                b = b + 5;
+                Console.WriteLine("sum : " + (a + b));
+                return a + b;
+
+            };
+            DoSomething(act); 
+            DoSomething(act);
+            act -= f1;
+            DoSomething(act);
+            //DoSomething((a, b) => Console.WriteLine(a + b));
+            //DoSomething(func2);
+
+            PrinterDelegate printerDelegate = Printbefore;
+            printerDelegate += PrinterAfter;
+            DoSomething(printerDelegate);
+            //printerDelegate("ayush", 9);
+
         }
     }
 }
---
+++ Practicec#basics/Program3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicec_basics
+{
+    public static class ExtendString
+    {
+        public static void PrintStringwithInfo(this string s, int n)
+        {
+            while (n-- > 0)
+            {
+                Console.WriteLine("your string is : "+s+ " , length : "+ s.Length) ;
+            }
+
+        }
+        public static void PrintList(this List<int> list) 
+        { 
+            foreach (int s in list) 
+            { 
+                Console.WriteLine(s);
+            } 
+        }
+    }
+
+    internal class Program3
+    {
+        public static void Main(string[] args)
+        {
+            string str = "printed using extension method";
+            str.PrintStringwithInfo(5);
+
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Func<int, int,int> aggreagtor = (a,b) => a +b;
+            Predicate<int> CheckEvenodd = a =>
+            {
+                if (a % 2 == 0)
+                {
+                    return true;
+                }
+                return false;
+            };
+           //int sum =  arr.Aggregate(aggreagtor);
+           List<int> list = new List<int>();
+            list.AddRange([87, 34, 88, 98, 7, 67, 56, 78, 5]);
+            
+           // Console.WriteLine(list.Find(CheckEvenodd));
+           list.FindAll(CheckEvenodd).PrintList();
+        }
+        
+       
+
+    }
+}
